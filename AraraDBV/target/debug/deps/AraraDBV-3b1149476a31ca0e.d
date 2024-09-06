b3545fa8f7335da37001e70e4c74d827e6403418
Thainan Luiz <thainandj@hotmail.com>
2024-09-05 22:09:09 -0300

    delete previous Arara crate

--- AraraDBV/target/debug/deps/AraraDBV-3b1149476a31ca0e.d
+++
@@ -1,5 +0,0 @@
-/home/bruno/Desktop/Projetos GitHub/AraraDBV/AraraDBV/target/debug/deps/libAraraDBV-3b1149476a31ca0e.rmeta: src/main.rs
-
-/home/bruno/Desktop/Projetos GitHub/AraraDBV/AraraDBV/target/debug/deps/AraraDBV-3b1149476a31ca0e.d: src/main.rs
-
-src/main.rs:
--- AraraDBV/target/debug/deps/AraraDBV-bab89c51dd7bf5b8.d
+++
@@ -1,5 +0,0 @@
-/home/bruno/Desktop/Projetos GitHub/AraraDBV/AraraDBV/target/debug/deps/libAraraDBV-bab89c51dd7bf5b8.rmeta: src/main.rs
-
-/home/bruno/Desktop/Projetos GitHub/AraraDBV/AraraDBV/target/debug/deps/AraraDBV-bab89c51dd7bf5b8.d: src/main.rs
-
-src/main.rs:
